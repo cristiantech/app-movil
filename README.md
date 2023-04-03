502d5b9fa259e4aad1859ad31e1de9cbca7bc630
CrislTech <cristian.munoz@iti.edu.ec>
2023-04-03 15:26:08 -0500

    Finalazación del CRUD

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-### Aplicaciones Moviles 
+# Aplicaciones Moviles 
+
+## Semana 1: Introudción a programación movil
++ Aplicaciones móviles y su importancia
++ Características y diferencias entre aplicaciones móviles nativas, híbridas y web
++ Introducción a las plataformas móviles iOS y Android
+
+## Semana 2 Introuducción a React Native
++ Historia de React
++ Instylación de React
++ Entorno de desarrollo para React
++ Creando Nuestro primer hola mundo
++ Explicando el proyecto a generar CRUD
+  + Manejo de estados React.useState()
+  + Agregar Evenetos onCLick, onSubmit, onChange
+  + Eliminar Mapeo y filtos de datos splice
+  + Editar Rederizar Operadores ternarios
+*Librerias a utlizar*
+shorId [Link de repositorio](https://yarnpkg.com/package/shortid)
+ 
+## Semana 3
--- src/App.js
+++ src/App.js
@@ -1,8 +1,110 @@
+import React, {useState} from 'react'
+import shortid from 'shortid'
+
 function App() {
+
+  const [task, setTask] = useState('')
+  const [tasks, setTasks] = useState([])
+  const [editMute, setEditMute] = useState(false)
+  const [id, setId] = useState('')
+
+  
+  const addTask = (e) => {
+     e.preventDefault()
+     if(!task.length > 0){
+       console.log('ok') // Agregar poput
+     }
+     const newTask = {
+      id: shortid.generate(),
+      name: task
+     }
+  
+     // agreagando tareas al array de atreas
+     setTasks([...tasks, newTask])
+     console.log(tasks);
+     setTask('')
+  }
+  const saveTask = (e) => {
+     e.preventDefault()
+     if(!task.length > 0){
+       console.log('Error') // Agregar un error
+     }
+    
+     const editeTasks = tasks.map(tk => tk.id === id ? {id, name: task}: tk)
+     // agreagando tareas al array de atreas
+     setTasks(editeTasks)
+     setEditMute(false)
+     setTask('')
+     setId('')
+  }
+
+  const deletedTask = (id) => {
+    const indexTask = tasks.findIndex(tk => tk.id === id);
+    tasks.splice(indexTask, 1)
+    const newStatck = [...tasks]
+    setTasks(newStatck)
+  }
+  const editTask = (editTask) => {
+    setTask(editTask.name)
+    setEditMute(true)
+    setId(editTask.id)
+  }
+
   return (
-    <div className="container mt-5">
+    <div className="container mt-5"> 
       <h1>Tareas</h1>
       <hr/>
+      <div className="row">
+        <div className="col-8">
+          <h4 className="text-center">Lista de tareas</h4>
+          {
+            (!tasks.length > 0) ? 
+            <h5 className='text-center'>No Hay Tareas Programadas</h5>
+            :
+            <ul className="list-group">
+              {
+                tasks.map(tk => (
+                  <li 
+                  className="list-group-item"
+                  key={tk.id}
+                  >
+                    <span>{tk.name}</span>
+                    
+                    <button 
+                    className="btn btn-warning btn-sm float-end mx-2"
+                    onClick={() => editTask(tk)}
+                    >Editar</button>
+                    
+                    <button 
+                    className="btn btn-danger btn-sm float-end"
+                    onClick={() => deletedTask(tk.id)}
+                    >Eliminar</button>
+                  </li>
+                ))
+              }
+            </ul>
+          }
+        </div>
+        <div className="col-4">
+          <h4 className="text-center">
+            { editMute ? `Editar tarea` : ` Agregar Tarea `}
+          </h4>
+          <form onSubmit={editMute ? saveTask : addTask}>
+            <input 
+            type="text"
+            className="form-control mb-2"
+            placeholder= {editMute ? `Actualizar tarea`: `Agregar tarea`}
+            onChange={(text) => {
+              setTask(text.target.value)
+            }}
+            value={task}
+            />
+            <button className={ editMute ? `btn btn-warning` : `btn btn-dark`}type="submit">
+              {editMute ? `Guardar`: `Agregar` }
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
